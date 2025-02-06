84b950b42736e4023663552b00c90832267063ba
xxli <43925398+v587dexinxin@users.noreply.github.com>
2025-02-07 00:05:58 +0800

    Create Fig_1D_K562_low_cells_APA_plot.sh

---
+++ Analysis/Fig_1D_K562_low_cells_APA_plot.sh
@@ -0,0 +1,12 @@
+java -jar /work/bio-shenw/tools/juicer_tools_1.21.01.jar apa /scratch/2024-04-15/bio-shenw/Ljniu/K562/K562_0.1_FA/workspace/results/matrix/K562_merged5.hg38.nodups.pairs.hic /scratch/2024-04-15/bio-shenw/Ljniu/K562/K562_0.1_FA/loops/ChIA-pipline_8K+/two_anchors_binding_loops/K562_merge5_0.1FA.hg38_peaks_two_anchors_binding_loops.bedpe ./ -r 10000 -u
+java -jar /work/bio-shenw/tools/juicer_tools_1.21.01.jar apa ../../../../K562_lowcells/workspace/results/pairs_library/K562_0.1FA_50k_merged2.hg38.nodups.pairs.hic ../../../../K562_lowcells/loops/ChIA-pipline_8K+/K562_50k/one_anchors_binding_loops/Selected_K562_lowcells_merged6.hg38_peaks_two_anchors_binding_loops_0.05.bedpe ./ -r 10000
+java -jar /work/bio-shenw/tools/juicer_tools_1.21.01.jar apa ../../../../K562_lowcells/workspace/results/pairs_library/K562_0.1FA_25k_merged2.hg38.nodups.pairs.hic ../../../../K562_lowcells/loops/ChIA-pipline_8K+/K562_50k/one_anchors_binding_loops/Selected_K562_lowcells_merged6.hg38_peaks_two_anchors_binding_loops_0.05.bedpe ./ -r 10000
+java -jar /work/bio-shenw/tools/juicer_tools_1.21.01.jar apa ../../../../K562_lowcells/workspace/results/pairs_library/K562_0.1FA_5k_merged2.hg38.nodups.pairs.hic ../../../../K562_lowcells/loops/ChIA-pipline_8K+/K562_50k/one_anchors_binding_loops/Selected_K562_lowcells_merged6.hg38_peaks_two_anchors_binding_loops_0.05.bedpe ./ -r 10000
+java -jar /work/bio-shenw/tools/juicer_tools_1.21.01.jar apa ../../../../K562_lowcells/workspace/results/pairs_library/K562_0.1FA_1k_merged2.hg38.nodups.pairs.hic ../../../../K562_lowcells/loops/ChIA-pipline_8K+/K562_50k/one_anchors_binding_loops/Selected_K562_lowcells_merged6.hg38_peaks_two_anchors_binding_loops_0.05.bedpe ./ -r 10000
+
+
+
+
+
+
+
