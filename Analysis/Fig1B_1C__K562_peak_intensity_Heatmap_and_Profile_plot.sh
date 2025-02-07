67e4b12acc629d27171c46c9906ec009d82c9bd4
xxli <43925398+v587dexinxin@users.noreply.github.com>
2025-02-07 10:19:24 +0800

    Update and rename Fig1B_1C_K562_peak_intensity_Heatmap_and_Profile_plot.sh to Fig1B_1C__K562_peak_intensity_Heatmap_and_Profile_plot.sh

--- Analysis/Fig1B_1C__K562_peak_intensity_Heatmap_and_Profile_plot.sh
+++ Analysis/Fig1B_1C__K562_peak_intensity_Heatmap_and_Profile_plot.sh
@@ -15,7 +15,10 @@ computeMatrix reference-point -S ../../../K562_0.1_FA/one-dimensional/mapping/al
 plotHeatmap -m K562_lowcells_union_1M_norm.gz -out K562_lowcells_union_1M_peaks_heatmap_norm.pdf --heatmapHeight 15 --refPointLabel peaks_center  --regionsLabel 1M_peaks --samplesLabel 1M 50k 25k 5k 1k --colorList white,red
 
 
-
+#####K562 Heatmap of signal intensity under different datasets.
+##The normalization method for peak intensity across different cell numbers can be found in the scripts folder.
+computeMatrix reference-point -S ../../../K562_0.1_FA/one-dimensional/mapping/all_reps/K562_0.1FA_allreps_RPKM_10bp.bw ../../../../../literature/K562/PRO_seq/hg38/mapping/K562_PRO_seq_SRR8137173_10bp.bw ../../../../../literature/K562/R_chip/mapping/bam2/K562_D210N_V5ChIP_SRR5379780_Rep1_RPKM_10bp.bw ../../../../../literature/K562/ChIP_seq/hg38/K562_POLR2A_hg38_ENCFF124WLE.bigWig -R ../../../K562_0.1_FA/one-dimensional/macs2/common_union/K562_0.1FA_onedimensional_q0.05_common_peaks.narrowPeak --referencePoint center -a 2000 -b 2000 -out K562_0.1FA_union.gz --skipZeros --missingDataAsZero
+plotHeatmap -m K562_0.1FA_union_normrpc.gz -out K562_0.1FA_PRO_RChip_RNAP2A_heatmap_normrpc.pdf --heatmapHeight 15 --refPointLabel peaks_center  --regionsLabel union_peaks --samplesLabel 0.1FA PRO-seq RChIP PolII --colorList white,red
 
 
 
